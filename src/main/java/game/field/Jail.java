04b37c1aeeef40eb5f01942e2915876c3ff4701b
Adam <adamharald440@gmail.com>
2021-01-04 22:40:12 +0100

    Started on adding more fields

---
+++ src/main/java/game/field/Jail.java
@@ -0,0 +1,15 @@
+package game.field;
+
+public class Jail extends Field{
+
+    private int goToJailPosition;
+
+    public Jail(String title, String subText, String description, int position, int goToJailPosition) {
+        super(title, subText, description, position);
+        this.goToJailPosition = goToJailPosition;
+    }
+
+    public void fieldAction() {
+        //TODO: Implement getting out of jail
+    }
+}
