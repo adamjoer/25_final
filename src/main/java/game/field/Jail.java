5332858dfb9d7a3b74bbc1e5bd4ac4cdb967ff9f
Adam <adamharald440@gmail.com>
2021-01-05 13:14:43 +0100

    Added bail attribute, that i forgot about

--- src/main/java/game/field/Jail.java
+++ src/main/java/game/field/Jail.java
@@ -2,11 +2,13 @@ package game.field;
 
 public class Jail extends Field{
 
+    private final int bail;
     private final int goToJailPosition;
 
-    public Jail(String title, String subText, String description, int position, int goToJailPosition) {
+    public Jail(String title, String subText, String description, int position, int bail, int goToJailPosition) {
         super(title, subText, description, position);
         this.goToJailPosition = goToJailPosition;
+        this.bail = bail;
     }
 
     public void fieldAction() {
