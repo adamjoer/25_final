f7b0a5e3aa4317b68207f4311b23844feb589f8e
Andreas Carlsen <s205437@student.dtu.dk>
2021-01-05 16:55:35 +0100

    Added constructor for PlayerController

--- src/main/java/game/PlayerController.java
+++ src/main/java/game/PlayerController.java
@@ -2,4 +2,16 @@ package game;
 
 public class PlayerController {
 
+    Player[] players;
+    private final int startBalance = 30000;
+
+
+    public PlayerController(String[] playerNames) {
+        players = new Player[playerNames.length];
+
+        for(int i = 0; i < playerNames.length; i++){
+            players[i] = new Player(playerNames[i], startBalance);
+        }
+
+    }
 }
