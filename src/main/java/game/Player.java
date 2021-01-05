4010867e77e46663a433894a827d353119a7ef30
Andreas Carlsen <s205437@student.dtu.dk>
2021-01-05 14:11:36 +0100

    Added variables and constructor to Player class

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -1,4 +1,16 @@
 package game;
 
 public class Player {
+    
+    private Account account;
+    private int oldPosition;
+    private int currentPosition;
+    private int[] properties = new int[30];
+    private final String NAME;
+
+
+    public Player(String NAME, int startBalance) {
+        account = new Account(startBalance);
+        this.NAME = NAME;
+    }
 }
