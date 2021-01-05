fe90e1439e2732094be4a16cff75c62c8be9d86a
Adam <adamharald440@gmail.com>
2021-01-05 17:51:01 +0100

    Revamped Property attributes to accommodate houses and hotels

--- src/main/java/game/field/Property.java
+++ src/main/java/game/field/Property.java
@@ -1,21 +1,40 @@
 package game.field;
 
 import java.awt.Color;
+import java.util.Arrays;
 
 public class Property extends Field {
 
     // Attributes
-    private final int value;
+    private final int cost;
+    private final int buildingCost;
+    private final int pawnValue;
     private final Color color;
-    private final int relatedPropertyPosition;
-    private int owner = -1;
+    private final int[] rentLevels;
+    private int propertyLevel = 0;
+    private final int relatedProperties;
+    private final int nextRelatedProperty;
+    private int owner = -1; // -1 means the property is owned by the bank
 
-    // Constructor
-    public Property(String title, String subText, String description, int position, int value, Color color, int relatedPropertyPosition) {
+    // The array rentLevels represents the cost of rent at different levels:
+    //      0: Base rent
+    //      1: Double rent
+    //      2: With one house built on it
+    //      3: With two houses built on it
+    //      4: With three houses built on it
+    //      5: With four houses built on it
+    //      6: With a hotel built on it
+
+    public Property(String title, String subText, String description, int position, int cost, int buildingCost, int pawnValue, Color color, int[] rent,
+                    int relatedProperties, int nextRelatedProperty) {
         super(title, subText, description, position);
-        this.value = value;
+        this.cost = cost;
+        this.buildingCost = buildingCost;
+        this.pawnValue = pawnValue;
         this.color = color;
-        this.relatedPropertyPosition = relatedPropertyPosition;
+        this.rentLevels = rent;
+        this.relatedProperties = relatedProperties;
+        this.nextRelatedProperty = nextRelatedProperty;
     }
 
     public void fieldAction() {
@@ -23,16 +42,36 @@ public class Property extends Field {
     }
 
     // Relevant getters
-    public int getValue() {
-        return value;
+    public int getCost() {
+        return cost;
+    }
+
+    public int getBuildingCost() {
+        return buildingCost;
+    }
+
+    public int getPawnValue() {
+        return pawnValue;
     }
 
     public Color getColor() {
         return color;
     }
 
-    public int getRelatedPropertyPosition() {
-        return relatedPropertyPosition;
+    public int[] getRentLevels() {
+        return rentLevels;
+    }
+
+    public int getPropertyLevel() {
+        return propertyLevel;
+    }
+
+    public int getRelatedProperties() {
+        return relatedProperties;
+    }
+
+    public int getNextRelatedProperty() {
+        return nextRelatedProperty;
     }
 
     public int getOwner() {
@@ -40,15 +79,24 @@ public class Property extends Field {
     }
 
     // Relevant setters
+    public void setPropertyLevel(int propertyLevel) {
+        this.propertyLevel = propertyLevel;
+    }
+
     public void setOwner(int owner) {
         this.owner = owner;
     }
 
     public String toString() {
         return super.toString() +
-               "\n\t[value=" + value +
-               "]\n\t[color=\"" + color.toString() +
-               "\"]\n\t[relatedPropertyPosition=" + relatedPropertyPosition +
+               "\n\t[cost=" + cost +
+               "]\n\t[buildingCost=" + buildingCost +
+               "]\n\t[pawnValue=" + pawnValue +
+               "]\n\t[Color=\"" + color.toString() +
+               "\"]\n\t[rentLevels=" + Arrays.toString(rentLevels) +
+               "]\n\t[propertyLevel=" + propertyLevel +
+               "]\n\t[relatedProperties=" + relatedProperties +
+               "]\n\t[nexRelatedProperty=" + nextRelatedProperty +
                "]\n\t[owner=" + owner + ']';
     }
 }
