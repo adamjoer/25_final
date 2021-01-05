25b564a810bfba79c86f5bfe47c7b40fce49459f
Adam <adamharald440@gmail.com>
2021-01-05 14:15:16 +0100

    Added method for checking if a player has passed go
    This could just be placed in the Board class. I dunno.

--- src/main/java/game/FieldController.java
+++ src/main/java/game/FieldController.java
@@ -17,6 +17,25 @@ public class FieldController {
         fields[position].fieldAction();
     }
 
+
+    /**
+     * Method for checking whether a player has passed the Go field,
+     * and is therefore eligible for the 'pass go' reward.
+     * This is based entirely upon the assumption that the Go field's
+     * position is zero on the board. It won't work otherwise.
+     *
+     * @param previousPosition The player's previous position
+     * @param currentPosition  The player's current position
+     * @return True if player has passed, or is on the 'Go' field, false otherwise
+     */
+    public boolean hasPassedGo(int previousPosition, int currentPosition) {
+        // FIXME: Should this just be placed in Board?
+
+        // If start field position is zero, player will have passed start if their position has overflowed to a smaller value,
+        // i.e. their previous position is larger than their current position
+        return previousPosition > currentPosition;
+    }
+
     // Relevant getters
     public Field[] getFields() {
         return fields;
