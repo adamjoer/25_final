464e07391ba4b3687a8f1e4d79e990eb3d0096f2
Andreas Carlsen <s205437@student.dtu.dk>
2021-01-06 11:12:34 +0100

    Added test for playercontroller

---
+++ src/test/java/game/PlayerControllerTest.java
@@ -0,0 +1,31 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerControllerTest {
+
+    PlayerController pc = new PlayerController(new String[]{"John", "Elise", "Jane", "Carsten"});
+
+    @Test
+    void makeTransaction() {
+
+    }
+
+    @Test
+    void movePlayer() {
+    }
+
+    @Test
+    void addProperty() {
+    }
+
+    @Test
+    void removeProperty() {
+    }
+
+    @Test
+    void giftPlayer() {
+    }
+}
