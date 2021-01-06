068965156ca3456913a10711437fe1a009db3a1f
Adam <adamharald440@gmail.com>
2021-01-06 18:55:26 +0100

    Made all ownable fields inherit from an abstract Property class which itself inherits from Field.

---
+++ src/main/java/game/field/Property.java
@@ -0,0 +1,80 @@
+package game.field;
+
+import java.awt.Color;
+import java.util.Arrays;
+
+public abstract class Property extends Field {
+
+    // Attributes
+    protected final int cost;
+    protected final int pawnValue;
+    protected final int[] rentLevels;
+    protected int propertyLevel = 0;
+    protected final int relatedProperties;
+    protected final int nextRelatedProperty;
+    protected int owner = -1; // -1 means the property is owned by the bank
+
+    public Property(String title, String subText, String description, int position, Color color, int cost, int pawnValue, int[] rentLevels, int relatedProperties, int nextRelatedProperty) {
+        super(title, subText, description, position, color);
+        this.cost = cost;
+        this.pawnValue = pawnValue;
+        this.rentLevels = rentLevels;
+        this.relatedProperties = relatedProperties;
+        this.nextRelatedProperty = nextRelatedProperty;
+    }
+
+    public abstract void fieldAction();
+
+    // Relevant getters
+    public int getCurrentRent() {
+        return rentLevels[propertyLevel];
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getPawnValue() {
+        return pawnValue;
+    }
+
+    public int[] getRentLevels() {
+        return rentLevels;
+    }
+
+    public int getPropertyLevel() {
+        return propertyLevel;
+    }
+
+    public int getRelatedProperties() {
+        return relatedProperties;
+    }
+
+    public int getNextRelatedProperty() {
+        return nextRelatedProperty;
+    }
+
+    public int getOwner() {
+        return owner;
+    }
+
+    // Relevant setters
+    public void setPropertyLevel(int propertyLevel) {
+        this.propertyLevel = propertyLevel;
+    }
+
+    public void setOwner(int owner) {
+        this.owner = owner;
+    }
+
+    public String toString() {
+        return super.toString() +
+               "\n\t[cost=" + cost +
+               "]\n\t[pawnValue=" + pawnValue +
+               "]\n\t[rentLevels=" + Arrays.toString(rentLevels) +
+               "]\n\t[propertyLevel=" + propertyLevel +
+               "]\n\t[relatedProperties=" + relatedProperties +
+               "]\n\t[nexRelatedProperty=" + nextRelatedProperty +
+               "]\n\t[owner=" + owner + ']';
+    }
+}
--- src/main/java/game/field/Street.java
+++ src/main/java/game/field/Street.java
@@ -1,40 +1,15 @@
 package game.field;
 
 import java.awt.Color;
-import java.util.Arrays;
 
-public class Street extends Field {
+public class Street extends Property {
 
     // Attributes
-    private final int cost;
     private final int buildingCost;
-    private final int pawnValue;
-    private final Color color;
-    private final int[] rentLevels;
-    private int propertyLevel = 0;
-    private final int relatedProperties;
-    private final int nextRelatedProperty;
-    private int owner = -1; // -1 means the property is owned by the bank
 
-    // The array rentLevels represents the cost of rent at different levels:
-    //      0: Base rent
-    //      1: Double rent
-    //      2: With one house built on it
-    //      3: With two houses built on it
-    //      4: With three houses built on it
-    //      5: With four houses built on it
-    //      6: With a hotel built on it
-
-    public Street(String title, String subText, String description, int position, int cost, int buildingCost, int pawnValue, Color color, int[] rent,
-                  int relatedProperties, int nextRelatedProperty) {
-        super(title, subText, description, position);
-        this.cost = cost;
+    public Street(String title, String subText, String description, int position, Color color, int cost, int pawnValue, int[] rentLevels, int relatedProperties, int nextRelatedProperty, int buildingCost) {
+        super(title, subText, description, position, color, cost, pawnValue, rentLevels, relatedProperties, nextRelatedProperty);
         this.buildingCost = buildingCost;
-        this.pawnValue = pawnValue;
-        this.color = color;
-        this.rentLevels = rent;
-        this.relatedProperties = relatedProperties;
-        this.nextRelatedProperty = nextRelatedProperty;
     }
 
     public void fieldAction() {
@@ -42,10 +17,6 @@ public class Street extends Field {
     }
 
     // Relevant getters
-    public int getCurrentRent() {
-        return rentLevels[propertyLevel];
-    }
-
     public int getNumberOfHouses() {
         return switch (propertyLevel) {
             case 0, 1, 6 -> 0;
@@ -59,61 +30,12 @@ public class Street extends Field {
         return 0;
     }
 
-    public int getCost() {
-        return cost;
-    }
-
     public int getBuildingCost() {
         return buildingCost;
     }
 
-    public int getPawnValue() {
-        return pawnValue;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public int[] getRentLevels() {
-        return rentLevels;
-    }
-
-    public int getPropertyLevel() {
-        return propertyLevel;
-    }
-
-    public int getRelatedProperties() {
-        return relatedProperties;
-    }
-
-    public int getNextRelatedProperty() {
-        return nextRelatedProperty;
-    }
-
-    public int getOwner() {
-        return owner;
-    }
-
-    // Relevant setters
-    public void setPropertyLevel(int propertyLevel) {
-        this.propertyLevel = propertyLevel;
-    }
-
-    public void setOwner(int owner) {
-        this.owner = owner;
-    }
-
     public String toString() {
         return super.toString() +
-               "\n\t[cost=" + cost +
-               "]\n\t[buildingCost=" + buildingCost +
-               "]\n\t[pawnValue=" + pawnValue +
-               "]\n\t[Color=\"" + color.toString() +
-               "\"]\n\t[rentLevels=" + Arrays.toString(rentLevels) +
-               "]\n\t[propertyLevel=" + propertyLevel +
-               "]\n\t[relatedProperties=" + relatedProperties +
-               "]\n\t[nexRelatedProperty=" + nextRelatedProperty +
-               "]\n\t[owner=" + owner + ']';
+               "\n\t[buildingCost=" + buildingCost + ']';
     }
 }
