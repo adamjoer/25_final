b0b48a22c9f95e1eed25f0d30ea5331d41daadac
Bastian <basbafala@gmail.com>
2021-01-06 14:02:21 +0100

    Example of HouseTax Chancecard added

---
+++ src/main/java/chance/card/HouseTax.java
@@ -0,0 +1,34 @@
+package chance.card;
+
+public class HouseTax extends ChanceCard {
+
+    public final int amount;
+    public final int cardnumber;
+    public static int increment;
+    public static int priceIncrease;
+    public static int destination;
+    public static boolean prison;
+
+    public static Instructions instructions;
+
+
+    /**
+     * @param CARD_TEXT
+     * @param amount
+     * @param cardnumber
+     */
+    public HouseTax(String CARD_TEXT, int amount, int cardnumber){
+        super(CARD_TEXT);
+        this.amount = amount;
+        this.cardnumber = cardnumber;
+        this.increment = 0;
+        this.priceIncrease = 0;
+        this.prison = false;
+
+    }
+
+    public Instructions executeCard(){
+        instructions = new Instructions(amount, cardnumber, increment, priceIncrease, destination, prison);
+        return instructions;
+    }
+}
