79444f76e60136d68578723890d2ead87530c49f
Bastian <basbafala@gmail.com>
2021-01-06 14:02:05 +0100

    Chancecard super class added

---
+++ src/main/java/chance/card/ChanceCard.java
@@ -0,0 +1,15 @@
+package chance.card;
+
+public class ChanceCard {
+
+    protected final String CARD_TEXT;
+
+    public ChanceCard(String CARD_TEXT) {
+        this.CARD_TEXT = CARD_TEXT;
+    }
+
+    public String getChanceCardText() {
+        return CARD_TEXT;
+    }
+
+}
