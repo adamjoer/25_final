ccd0b430e8910e4bcce3c936db7d27fd8857dd88
Azeliah <s205460@student.dtu.dk>
2021-01-07 15:08:34 +0100

    BankTransaction ChanceCard

--- src/main/java/chance/card/BankTransaction.java
+++ src/main/java/chance/card/BankTransaction.java
@@ -3,34 +3,20 @@ package chance.card;
 public class BankTransaction extends ChanceCard {
 
     public final int amount;
-    public final int cardnumber;
-    public static int increment;
-    public static int houseTax;
-    public static int destination;
     public static int player;
-    public static boolean prison;
 
-    public static Instructions instructions;
 
     /**
      * @param CARD_TEXT
      * @param amount
-     * @param cardnumber
      */
-    public BankTransaction(String CARD_TEXT, int amount, int cardnumber, int player){
+
+    public BankTransaction(String CARD_TEXT, int amount){
         super(CARD_TEXT);
         this.amount = amount;
-        this.cardnumber = cardnumber;
-        this.player = player;
-        this.increment = 0;
-        this.houseTax = 0;
-        this.prison = false;
-
     }
 
-    public Instructions executeCard(){
-        instructions = new Instructions(cardnumber, amount, increment, destination, houseTax, player, prison);
-        return instructions;
+    public CardInstruction executeCard(){
+        CardInstruction instruction = new CardInstruction(getClass().getSimpleName(),amount);
     }
-
 }
