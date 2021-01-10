41547faff90c27151a2ca7c2ddafe2528fefd65c
Azeliah <s205460@student.dtu.dk>
2021-01-10 22:10:29 +0100

    JavaDoc written in ChanceCardController.

--- src/main/java/game/chance/card/ChanceCardController.java
+++ src/main/java/game/chance/card/ChanceCardController.java
@@ -10,14 +10,10 @@ public class ChanceCardController {
     private final int BASE_DECK_LENGTH;
     private ChanceCard[] drawPile;
     private int pilePosition = -1;
-/*
-    public static void main(String[] arg){
-        ChanceCardController chanceCardController = new ChanceCardController();
-    }
-*/
 
     /**
-     *
+     * Constructor. Executes chanceCardGenerator in game.Utility to create a base deck.
+     * Proceeds to make a drawPile with references to the original deck in random order.
      */
 
     public ChanceCardController(){
@@ -28,10 +24,10 @@ public class ChanceCardController {
     }
 
     /**
-     *
-     * @param array
-     * @param n
-     * @return
+     * appendIntArray creates a new int array with one more place and fills in that place with a given integer.
+     * @param array The array to add a place to.
+     * @param n The value to assign to the created place.
+     * @return The array with the appended int place (placed at the last index).
      */
 
     private int[] appendIntArray(int[] array, int n){
@@ -42,10 +38,10 @@ public class ChanceCardController {
     }
 
     /**
-     *
-     * @param array
-     * @param index
-     * @return
+     * removeIntArrayByIndex creates a new array with one less place than the input array. Specified index is removed.
+     * @param array The int array from which a place is to be removed.
+     * @param index The index of the place to be removed.
+     * @return An int array with one less place - the specified index removed.
      */
     private int[] removeIntArrayByIndex(int[] array, int index){
         int[] result = new int[array.length - 1];
@@ -55,9 +51,14 @@ public class ChanceCardController {
     }
 
     /**
-     *
-     * @param array
-     * @return
+     * shuffleIntArray utilizes removeIntArrayByIndex to remove a random element from the int array given and
+     * appendIntArray to apply the same random element to the end of the array.
+     * The method is recursive, such that it keeps on removing elements in a random order, until there is only one
+     * element left in the initial array. It then appends the single element arrays in the random order they were
+     * extracted. This guarantees a shuffled array. As the method picks out by index rather than by value, it ensures
+     * that the recursion is well defined.
+     * @param array The int array to shuffle
+     * @return A shuffled int array with the same elements as the argument given, in a random order.
      */
 
     private int[] shuffleIntArray(int[] array){
@@ -71,7 +72,10 @@ public class ChanceCardController {
     }
 
     /**
-     *
+     * shuffleDeck creates an int array with the same length as BASE_DECK and makes it so the each element of the int
+     * array corresponds to its index. The int array is then shuffled with shuffleIntArray and used as a shuffle
+     * reference for the drawPile so that each element in the drawPile has a reference to a different element in the
+     * BASE_DECK.
      */
 
     private void shuffleDeck(){
@@ -84,8 +88,10 @@ public class ChanceCardController {
     }
 
     /**
-     *
-     * @return
+     * drawCard increases the value of pilePosition by 1 and then returns the String of the corresponding card in the
+     * drawPile. If the last card in the drawPile is reached before the increment of pilePosition, the drawPile is
+     * shuffled and pilePosition is reset to 0.
+     * @return The String identifying the card type of the current ChanceCard.
      */
 
     public String drawCard(){
